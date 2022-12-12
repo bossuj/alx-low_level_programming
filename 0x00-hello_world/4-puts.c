727bf491da2df8ac2ed279a3de8b1dfda2994592
Collins <bossbtc16@gmail.com>
2022-12-12 12:47:20 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-/**                                                                                                                                     
+/**
  *
- *  * main - Entry                                                                                                                         
+ * *main - Entry point
  *
- *   * Return: Always 0                                                                                                                     
+ * *Return: Always 0 (Success)
  *
- *    */                                                                                                                                     
+ * */
 
-int main(void)                                                                                                                          
 
-{                                                                                                                                       
 
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
+int main(void)
 
-			        return (0);
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+					return (0);
 
 }
