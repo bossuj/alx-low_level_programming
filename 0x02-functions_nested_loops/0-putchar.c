18867960a0322c34d46e1b3c0e79628e15ceab44
Collins <bossbtc16@gmail.com>
2022-12-14 12:04:07 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,6 +2,7 @@
 
 /**
  * main - Entry point
+ *
  * Return: 0 nothing, return void
  */
 
@@ -9,21 +10,13 @@ int main(void)
 
 {
 	_putchar('_');
-
 	_putchar('p');
-
 	_putchar('u');
-
 	_putchar('t');
-
 	_putchar('c');
-
 	_putchar('h');
-
 	_putchar('a');
-
 	_putchar('r');
-
 	_putchar('\n');
 
 	return (0);
